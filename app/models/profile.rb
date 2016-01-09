e5b141d96d9788c296f0f3ab98438a772ca286f9
John Owuor <owuor91@gmail.com>
2016-01-09 15:37:33 +0300

    add custom stylesheets

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
+  #before_filter :signed_in_user, only: [:index]
   belongs_to :user
 
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' },
@@ -7,4 +8,5 @@ class Profile < ActiveRecord::Base
                              hash_secret: '<get_use_rake_secret>'
   validates_attachment :avatar, content_type: { content_type: /\Aimage\/.*\Z/ },
                                 size: { in: 0..1.megabytes }
+
 end
