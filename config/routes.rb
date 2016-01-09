a63ec403cfda4807d97057f96d343af71e7cad72
John Owuor <owuor91@gmail.com>
2016-01-09 12:15:51 +0300

    add custom fields to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
