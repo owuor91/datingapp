2d901ed5ce9aed3d7a5a039fcfe09f8736e9efa2
John Owuor <owuor91@gmail.com>
2016-01-11 21:38:18 +0300

    save user gender and id to db

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :profiles
-
   get ':id' => 'users#show', as: :user_profile
   get ':id/setting' => 'users#edit', as: :user_setting
   match ':id/setting' => 'users#update', via: [:put, :patch]
