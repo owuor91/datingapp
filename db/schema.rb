34771fa82f6c1b580ee910735556539e8ba279e5
John Owuor <owuor91@gmail.com>
2016-01-16 12:49:12 +0300

    Revert "install omniauth and create facebook app"
    This reverts commit 4e40cd7094663cb98406617fc00f26fdd042e992.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113150848) do
+ActiveRecord::Schema.define(version: 20160109094540) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id",             limit: 4
@@ -46,14 +46,10 @@ ActiveRecord::Schema.define(version: 20160113150848) do
     t.string   "name",                   limit: 255
     t.integer  "age",                    limit: 4
     t.string   "sex",                    limit: 255
-    t.string   "provider",               limit: 255
-    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
   add_foreign_key "profiles", "users"
 end
