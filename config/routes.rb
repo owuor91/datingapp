4cf609a6b425ec1d377e58b0b3331ed01c6e8edf
John Owuor <owuor91@gmail.com>
2016-01-16 12:47:55 +0300

    Revert "facebook signup dialog"
    This reverts commit 3f967657d1bf38c130d6a3071399c545370b6203.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :users
-  devise_for :users, :controllers => { registrations: 'registrations', :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'welcome/index'
 
   resources :profiles
