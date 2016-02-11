f5f9c151f1feaad3e88f2584b5318a3f09c7d75a
owuor91 <owuor91@gmail.com>
2016-02-11 17:04:14 +0300

    add placeholders for missing images

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,27 +2,27 @@ class Profile < ActiveRecord::Base
   #before_filter :signed_in_user, only: [:index]
   belongs_to :user
 
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: "/images/avatarph.png"
   validates_attachment :avatar, content_type: { content_type: /\Aimage\/.*\Z/ },
                                 size: { in: 0..1.megabytes }
 
-  has_attached_file :photo1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/avatarph.png"
+  has_attached_file :photo1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/camplaceholder.png"
   validates_attachment_content_type :photo1, content_type: /\Aimage\/.*\Z/
 
 
-  has_attached_file :photo2, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo2, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/camplaceholder.png"
   validates_attachment_content_type :photo2, content_type: /\Aimage\/.*\Z/
 
 
-  has_attached_file :photo3, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo3, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/camplaceholder.png"
   validates_attachment_content_type :photo3, content_type: /\Aimage\/.*\Z/
 
 
-  has_attached_file :photo4, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo4, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/camplaceholder.png"
   validates_attachment_content_type :photo4, content_type: /\Aimage\/.*\Z/
 
 
-  has_attached_file :photo5, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo5, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/camplaceholder.png"
   validates_attachment_content_type :photo5, content_type: /\Aimage\/.*\Z/
 
 end
