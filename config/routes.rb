daff37f91fb5f0f04041358f000b0c65f3c1f351
owuor91 <owuor91@gmail.com>
2016-02-19 14:19:50 +0300

    add chat styles and routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
+   resources :conversations do
+    resources :messages
+  end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
