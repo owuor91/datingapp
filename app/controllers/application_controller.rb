67c12dd857574daeaedf1b355a74198e89e72750
owuor91 <owuor91@gmail.com>
2016-03-05 14:28:54 +0300

    link to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def after_sign_in_path_for(resource)
-    profile_path(@user.id)
-  end
+  # def after_sign_in_path_for(resource)
+  #   profile_path(@user.id)
+  # end
 end
