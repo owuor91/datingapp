9f8c36176153bc8fceda48135dc24e8794329cdd
owuor91 <owuor91@gmail.com>
2016-03-08 19:19:55 +0300

    js for results display

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,7 +3,23 @@ var ready = function (){
     e.preventDefault();
 
     $('.dash').hide();
+
+    $('.searchbar').show();
   });
+
+  $('.searchbtn').click(function(e){
+    e.preventDefault();
+
+    $('.searchbar').hide();
+
+    $('.dash').show();
+
+    $('.dash > .container-fluid > .row').hide();
+
+    $('.result-title').text('Showing results for');
+  });
+
+
 
 }
 $(document).ready(ready);
