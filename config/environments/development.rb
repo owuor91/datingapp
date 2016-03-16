4218df4216665b560d73d5848b25086aacfb06d7
owuor91 <owuor91@gmail.com>
2016-03-16 14:11:03 +0300

    configuration for sending email from gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,20 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
-  
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "xivhelp@gmail.com",
+    password: "xiv@254KE"
+  }
+
+
 end
