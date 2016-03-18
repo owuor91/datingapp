2f19d17eca24191883a0ed63ab59600f5b082c23
owuor91 <owuor91@gmail.com>
2016-03-18 09:31:20 +0300

    add gmail smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'xivapp.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "xivhelp@gmail.com",
+    password: "xiv@254KE"
+  }
 end
